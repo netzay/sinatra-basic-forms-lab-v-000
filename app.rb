c1cf6ef10241cba83c33ed917c12d6697381e47e
Ally Netzel <netzelally@gmail.com>
2018-06-02 14:22:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
+require "pry"
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+      erb :index
+    end
 
+    get '/new' do
+      erb :create_puppy
+    end
+
+    post '/' do
+      #binding.pry
+    puppy = Puppy.new
+      @add_puppy = puppy(params[:name, :breed, :age])
+      erb :display_puppy
+    end
 end
